60331bd40ca765828617b6c4372b42ee1848bae8
john-daka28 <johndaka021@gmail.com>
2026-07-17 09:59:14 +0200

    Update navigation link text to include 'Academy'

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -33,7 +33,7 @@ export function Navigation() {
             <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary text-primary-foreground">
               <GraduationCap className="h-6 w-6" />
             </div>
-            <span className="hidden sm:inline">GlobeDk Elite</span>
+            <span className="hidden sm:inline">GlobeDk Elite Academy</span>
           </Link>
 
           {/* Desktop Navigation */}
